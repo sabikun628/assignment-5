69ac694938ebbaac7b9407d0667f580ca8c9acd7
sabikun628 <sabikun0628@gmail.com>
2024-02-18 13:31:33 +0600

    banner section added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primaryGreen: '#2f9d1e',
+      },
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+}
